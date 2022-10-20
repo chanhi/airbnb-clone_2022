c3a995518a3e799f31b067ed7ad65ad142d2040f
chanhi <61691088+chanhi@users.noreply.github.com>
2022-10-20 19:41:00 +0900

    ~#8 Power Admin

--- reviews/admin.py
+++ reviews/admin.py
@@ -2,6 +2,49 @@ from django.contrib import admin
 from .models import Review
 
 
+class WordFilter(admin.SimpleListFilter):
+
+    title = "Filter by words!"
+
+    parameter_name = "word"
+
+    def lookups(self, request, model_admin):
+        return [
+            ("good", "Good"),
+            ("great", "Great"),
+            ("awesome", "Awesome"),
+        ]
+
+    def queryset(self, request, reviews):
+        word = self.value()
+        if word:
+            return reviews.filter(payload__contains=word)
+        else:
+            reviews
+
+
+class RatingFilter(admin.SimpleListFilter):
+
+    title = "Filter by rating"
+
+    parameter_name = "rating"
+
+    def lookups(self, request, model_admin):
+        return [
+            ("good", "Good Review"),
+            ("bad", "Bad Review"),
+        ]
+
+    def queryset(self, request, reviews):
+        rating = self.value()
+        if rating == "good":
+            return reviews.filter(rating__gte=3)
+        elif rating == "bad":
+            return reviews.filter(rating__lt=3)
+        else:
+            return reviews
+
+
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
 
@@ -9,4 +52,11 @@ class ReviewAdmin(admin.ModelAdmin):
         "__str__",
         "payload",
     )
-    list_filter = ("rating",)
+    list_filter = (
+        WordFilter,
+        RatingFilter,
+        "rating",
+        "user__is_host",
+        "room__category",
+        "room__pet_friendly",
+    )
